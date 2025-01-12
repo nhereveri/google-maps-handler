0ba02d300b2bc50ad13baaca19b953ca0ca141bc
Nelson Hereveri <nelson@hereveri.cl>
2025-01-12 20:43:03 -0300

    AdvancedMarkerElement extensions

---
+++ src/marker-extensions.js
@@ -0,0 +1,21 @@
+export function extendAdvancedMarkerElement(iconManager) {
+    if (!("toggleLabel" in google.maps.marker.AdvancedMarkerElement.prototype)) {
+        google.maps.marker.AdvancedMarkerElement.prototype.toggleLabel = function (state) {
+            if (!this.labelElement) {
+                const label = document.createElement("div");
+                label.className = "marker-label";
+
+                const text = document.createElement("span");
+                text.className = "marker-label-text";
+                text.textContent = this.title || "no title";
+                label.appendChild(text);
+
+                this.labelElement = label;
+                this.content.appendChild(label);
+                console.log(this.content)
+            }
+
+            this.labelElement.style.display = state ? "block" : "none";
+        };
+    }
+}
